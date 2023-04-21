7ce4ffec88c60900b97212d5e8f81ab691636185
aliu19 <liu.and@northeastern.edu>
2023-04-21 14:53:19 -0400

    clean up code
    readability

--- trip - Copy.sql
+++ trip - Copy.sql
@@ -1,7 +1,8 @@
 DROP DATABASE IF EXISTS trips;
 CREATE DATABASE IF NOT EXISTS trips;
-use trips;
+USE trips;
 
+-- tables
 CREATE TABLE IF NOT EXISTS user (
 	username VARCHAR(32) PRIMARY KEY,
 	password VARCHAR(32) NOT NULL,
@@ -56,8 +57,8 @@ CREATE TABLE IF NOT EXISTS plans(
 
 CREATE TABLE IF NOT EXISTS repay (
 	expenseID INT NOT NULL,
-	payer varchar(32) NOT NULL,
-	owedTo varchar(32) NOT NULL,
+	payer VARCHAR(32) NOT NULL,
+	owedTo VARCHAR(32) NOT NULL,
 	amount DOUBLE NOT NULL,
 	transactionCompleted BIT(1) NOT NULL,
 	PRIMARY KEY(expenseID, payer, owedTo),
@@ -66,135 +67,148 @@ CREATE TABLE IF NOT EXISTS repay (
 	FOREIGN KEY (owedTo) REFERENCES user(username) ON DELETE CASCADE
 );
 
+-- user procedures
 DELIMITER $$
 CREATE PROCEDURE create_user (
-username_p varchar(32),
-password_p varchar(32),
-firstName_p varchar(32),
-lastName_p varchar(32),
-email_p varchar(32))
+	username_p VARCHAR(32),
+	password_p VARCHAR(32),
+	firstName_p VARCHAR(32),
+	lastName_p VARCHAR(32),
+	email_p VARCHAR(32)
+)
 BEGIN 
-INSERT INTO user VALUES(username_p, password_p, firstName_p, lastName_p, email_p);
+	INSERT INTO user VALUES(username_p, password_p, firstName_p, lastName_p, email_p);
 END$$
 DELIMITER ;
 
 DELIMITER $$
 CREATE PROCEDURE update_user_password (
-username_p varchar(32),
-password_p varchar(32))
+	username_p VARCHAR(32),
+	password_p VARCHAR(32)
+)
 BEGIN 
-UPDATE user
-SET password = password_p
-WHERE username = username_p;
+	UPDATE user
+	SET password = password_p
+	WHERE username = username_p;
 END$$
 DELIMITER ;
 
 DELIMITER $$
 CREATE PROCEDURE update_user_lastName (
-username_p varchar(32),
-lastName_p varchar(32))
+	username_p VARCHAR(32),
+	lastName_p VARCHAR(32)
+)
 BEGIN 
-UPDATE user
-SET lastName = lastName_p
-WHERE username = username_p;
+	UPDATE user
+	SET lastName = lastName_p
+	WHERE username = username_p;
 END$$
 DELIMITER ;
 
 DELIMITER $$
 CREATE PROCEDURE update_user_firstName (
-username_p varchar(32),
-firstName_p varchar(32))
+	username_p VARCHAR(32),
+	firstName_p VARCHAR(32)
+)
 BEGIN 
-UPDATE user
-SET firstName = firstName_p
-WHERE username = username_p;
+	UPDATE user
+	SET firstName = firstName_p
+	WHERE username = username_p;
+END$$
+DELIMITER ;
+
+-- trip procedures
+DELIMITER $$
+CREATE PROCEDURE create_trip (
+	city_p VARCHAR(32),
+	country_p VARCHAR(32),
+	tripName_p VARCHAR(32),
+	description_p VARCHAR(32),
+	startDate_p DATE, 
+	endDate_p DATE,
+	owner_p varchar(32)
+)
+BEGIN 
+	INSERT INTO trip (tripNAME, description, city, country, startDATE, endDATE, owner)
+	VALUES (tripNAME_p, description_p, city_p, country_p, startDATE_p, endDATE_p, owner_p);
 END$$
 DELIMITER ;
 
 DELIMITER $$
 CREATE PROCEDURE update_trip_name (
-tripID_p INT,
-tripName_p varchar(32))
+	tripID_p INT,
+	tripName_p VARCHAR(32)
+)
 BEGIN 
-UPDATE trip
-SET tripName = tripName_p
-WHERE tripID = tripID_p;
+	UPDATE trip
+	SET tripName = tripName_p
+	WHERE tripID = tripID_p;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE update_trip_description (
+	tripID_p INT,
+	description_p VARCHAR(32)
+)
+BEGIN 
+	UPDATE trip
+	SET description = description_p
+	WHERE tripID = tripID_p;
 END$$
 DELIMITER ;
 
 DELIMITER $$
 CREATE PROCEDURE update_trip_city (
-tripID_p INT,
-city_p varchar(32))
+	tripID_p INT,
+	city_p VARCHAR(32)
+)
 BEGIN 
-UPDATE trip
-SET city = city_p
-WHERE tripID = tripID_p;
-END$$
-DELIMITER ;
-
-DELIMITER $$
-CREATE PROCEDURE update_trip_desc (
-tripID_p INT,
-description_p varchar(32))
-BEGIN 
-UPDATE trip
-SET description = description_p
-WHERE tripID = tripID_p;
+	UPDATE trip
+	SET city = city_p
+	WHERE tripID = tripID_p;
 END$$
 DELIMITER ;
 
 DELIMITER $$
 CREATE PROCEDURE update_trip_country (
-tripID_p INT,
-country_p varchar(32))
+	tripID_p INT,
+	country_p VARCHAR(32)
+)
 BEGIN 
-UPDATE trip
-SET country = country_p
-WHERE tripID = tripID_p;
+	UPDATE trip
+	SET country = country_p
+	WHERE tripID = tripID_p;
 END$$
 DELIMITER ;
 
 DELIMITER $$
 CREATE PROCEDURE update_trip_start_date (
-tripID_p INT,
-startDate_p DATE)
+	tripID_p INT,
+	startDate_p DATE
+)
 BEGIN 
-UPDATE trip
-SET startDate = startDate_p
-WHERE tripID = tripID_p;
+	UPDATE trip
+	SET startDate = startDate_p
+	WHERE tripID = tripID_p;
 END$$
 DELIMITER ;
 
 DELIMITER $$
 CREATE PROCEDURE update_trip_end_date (
-tripID_p INT,
-endDate_p DATE)
+	tripID_p INT,
+	endDate_p DATE
+)
 BEGIN 
-UPDATE trip
-SET endDate = endDate_p
-WHERE tripID = tripID_p;
+	UPDATE trip
+	SET endDate = endDate_p
+	WHERE tripID = tripID_p;
 END$$
 DELIMITER ;
 
-DELIMITER $$
-CREATE PROCEDURE create_trip (
-city_p varchar(32),
-country_p varchar(32),
-tripName_p varchar(32),
-description_p varchar(32),
-startDate_p DATE, 
-endDate_p DATE,
-owner_p varchar(32))
-BEGIN 
-INSERT INTO trip (tripNAME, description, city, country, startDATE, endDATE, owner)
-VALUES (tripNAME_p, description_p, city_p, country_p, startDATE_p, endDATE_p, owner_p);
-END$$
-DELIMITER ;
-
+-- trip triggers
 DELIMITER  $$
-CREATE TRIGGER add_attendee_after_adding_trip
-AFTER INSERT 
+CREATE TRIGGER add_attendee_after_adding_trip AFTER INSERT 
 ON trip
 FOR EACH ROW
 BEGIN
@@ -204,46 +218,47 @@ DELIMITER ;
 
 DELIMITER $$
 CREATE PROCEDURE get_trip_info (
-tripID_p INT)
+	tripID_p INT
+)
 BEGIN 
-SELECT trip.*, GROUP_CONCAT(username)
-FROM trip 
-JOIN attends
-USING(tripID)
-GROUP BY tripID
-HAVING tripID = tripID_p;
+	SELECT trip.*, GROUP_CONCAT(username) FROM trip 
+	JOIN attends USING(tripID)
+	GROUP BY tripID
+	HAVING tripID = tripID_p;
 END$$
 DELIMITER ;
 
 DELIMITER $$
 CREATE PROCEDURE get_owner_active_trips (
-username_p varchar(32))
+	username_p VARCHAR(32)
+)
 BEGIN 
-SELECT tripID, tripName FROM trip
-WHERE owner = username_p;
+	SELECT tripID, tripName FROM trip
+	WHERE owner = username_p;
 END$$
 DELIMITER ;
 
 DELIMITER $$
 CREATE PROCEDURE add_person_to_trip (
-tripID_p INT,
-username_p varchar(32))
+	tripID_p INT,
+	username_p VARCHAR(32)
+)
 BEGIN 
-INSERT INTO attends (tripID, username) VALUES (tripID_p, username_p);
+	INSERT INTO attends (tripID, username) VALUES (tripID_p, username_p);
 END$$
 DELIMITER ;
 
 DELIMITER $$
 CREATE PROCEDURE check_login_exists (
-username_p varchar(32),
-password_p varchar(32))
+	username_p varchar(32),
+	password_p varchar(32)
+)
 BEGIN 
-SET @numRows = (SELECT COUNT(*) FROM(
-SELECT username, password
-FROM user
-WHERE username = username_p AND password = password_p) AS numRowsRS);
+	SET @numRows = (SELECT COUNT(*) FROM (
+			SELECT username, password FROM user
+			WHERE username = username_p AND password = password_p
+        ) AS numRowsRS);
 
-SELECT @numrows;
-
+	SELECT @numrows;
 END$$
 DELIMITER ;
